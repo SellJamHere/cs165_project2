a0a43f1808734fcb0e6cc2cf061c5664bc85d5e3
Nicole <nbuck@uci.edu>
2014-05-06 19:17:40 -0700

    working on Random

---
+++ Random.h
@@ -0,0 +1,24 @@
+//
+//  File.h
+//  CS165Project2
+//
+//  Created by Nicole Buck on 5/6/14.
+//  Copyright (c) 2014 Nicole Buck. All rights reserved.
+//
+
+#ifndef __CS165Project2__File__
+#define __CS165Project2__File__
+
+#include <iostream>
+#include "BigInt.h"
+
+class Random
+{
+    public:
+    
+    int getRandom(long input);
+    
+    private:
+};
+
+#endif /* defined(__CS165Project2__File__) */
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "BigInt.h"
+#include "Random.h"
 #include <string>
 
 using namespace std;
@@ -28,7 +29,7 @@ int main()
     // greater--;
     cout << (lesser < greater ? "YES" : "NO") << endl;
 
-    BigInt bigInt4("46");
+    BigInt bigInt4("400");
     BigInt bigInt5("23");
     BigInt bigMult = bigInt4 * bigInt5;
     cout << bigInt4 << " * " << bigInt5 << " = " << bigMult << endl;
@@ -37,7 +38,7 @@ int main()
     cout << bigInt4 << " % " << bigInt5 << " = " << bigMult << endl;
 
     BigInt base(67);
-    BigInt exponent(1000003);
+    BigInt exponent(34);
 //    cout << base << " ^ " << exponent << " = " << bigPow(base, exponent) << endl;
     cout << "GCD(" << base << ", " << exponent << ") = " << gcd(base, exponent) << endl;
     cout << "Make sure base(" << base << ") and exponent(" << exponent << ") did not change\n";
@@ -45,6 +46,18 @@ int main()
     cout << "J: " << J(base, exponent) << endl;
     
     cout << "Steven: " << Steven(base, exponent) << endl;
+    
+    
+    //Random number generator
+    Random random;
+    int rando = random.getRandom(20);
+    for (int i = 0; i<20; i++)
+    {
+        rando = random.getRandom(0);
+        cout << "Random number: " << rando << endl;
+    }
+    cout << "Random number: " << rando << endl;
+    
 
     return 0;
 }
